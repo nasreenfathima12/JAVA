269d5394e8f6a5273434ac23b089f75853efbd8e
nasreenfathima12 <166802259+nasreenfathima12@users.noreply.github.com>
2024-06-26 22:27:16 +0530

    Main

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        System.out.println("Addition :"+(a+b));
+        System.out.println("Sub :"+(a-b));
+        System.out.println("Division :"+(a/b));
+        System.out.println("multiplication :"+(a*b));
+        System.out.println("modulo :"+(a%b));
+    }
+}
+
+
+//Output:
+/*Addition :15
+Sub :5
+Division :2       
+multiplication :50
+modulo :0 */
