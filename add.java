b82adf35732cf8631990636b2eef92aeab55ff66
nasreenfathima12 <166802259+nasreenfathima12@users.noreply.github.com>
2024-06-28 19:06:14 +0530

    ADD

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class add {
+    public static void main(String[] args){
+        int x=5;
+        x+=3;
+        System.out.print("print the sum value");
+        System.out.println(x);
+    }
+    
+}
+
+
+/*OUTPUT
+print the sum value:
+8*/
