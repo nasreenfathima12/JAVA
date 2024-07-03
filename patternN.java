e97ca232a02504655e343842fc5b39875c16531e
nasreenfathima12 <166802259+nasreenfathima12@users.noreply.github.com>
2024-07-03 12:48:10 +0530

    patternN

---
+++ patternN.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class patternN {
+    
+        public static void main(String[] args){
+            int n=9;
+            for(int i=0;i<n;i++,System.out.println())
+            for(int j=0;j<n;j++)
+            if( i==j|| j==n-1 || j==0 )
+            System.out.print("*  ");
+            else
+            System.out.print("   ");
+    
+        }
+}   
+
+/*OUTPUT
+
+*                       *  
+*  *                    *
+*     *                 *
+*        *              *
+*           *           *
+*              *        *
+*                 *     *
+*                    *  *
+*                       *  
+
+ */
+    
+    
+    
+
