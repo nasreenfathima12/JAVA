bee07d240851e4b0c89098441263770c47e28965
nasreenfathima12 <166802259+nasreenfathima12@users.noreply.github.com>
2024-07-03 12:58:03 +0530

    patternR

---
+++ patternR.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class patternR {
+    public static void main(String[] args){
+        int n=9;
+        for(int i=0;i<n;i++,System.out.println())
+        for(int j=0;j<n;j++)
+        if(i==0||i==n/2&&j>=n/2||j==0||j==n-1&&i<=n/2||i==j&&j>=n/2)
+        System.out.print("*  ");
+        else
+        System.out.print("   ");
+
+    }
+}   
+    
+/*OUTOUT
+
+ *  *  *  *  *  *  *  *  *  
+*                       *
+*                       *
+*                       *
+*           *  *  *  *  *
+*              *
+*                 *
+*                    *
+*                       *       */
+ 
